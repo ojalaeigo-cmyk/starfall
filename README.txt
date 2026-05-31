dbe57abb99c8791bc184abc4ec1d4deb514887d8
ojalaeigo-cmyk <ojalaeigo@gmail.com>
2026-05-31 22:37:12 +0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,2 +1,5 @@
-STAR KING WORKING REBUILD
-Upload these files directly to repo root.
+STAR KING SPECIAL STARS + BOSS 3X
+- Special colored stars show what was achieved.
+- Yellow star has no extra text.
+- Boss HP is 3x stronger.
+- During boss phase, speed is slowed 3x.
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='star-king-working-rebuild-v1';
+const CACHE='star-king-special-stars-boss3x-v2';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
